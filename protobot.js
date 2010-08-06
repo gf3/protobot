9c36923c8b44e4c52c7d6cddec6c15cfc5c5d66b
Gianni Chiappetta <gianni@runlevel6.org>
2010-08-06 16:19:45 -0400

    updated: Styles. Added option to choose google search result.

--- protobot.js
+++ protobot.js
@@ -1,8 +1,8 @@
 /* ------------------------------ Includes && Options ------------------------------ */
-var sys = require('sys')
-  , jerk = require('./vendor/Jerk/lib/jerk')
-  , Sandbox =  require("./vendor/sandbox/lib/sandbox")
-  , Google = require("./vendor/google/google")
+var sys = require( 'sys' )
+  , jerk = require( './vendor/Jerk/lib/jerk' )
+  , Sandbox =  require( './vendor/sandbox/lib/sandbox' )
+  , Google = require( './vendor/google/google' )
   , options
   , commands
   , wat
@@ -10,10 +10,10 @@ var sys = require('sys')
 
 options = 
   { server:   'irc.freenode.net'
-  , nick:     'protobot'
+  , nick:     'david_mark'
   , channels: ['#runlevel6', '#prototype', '#jquery-ot']
   , user:
-    { username: 'protobot'
+    { username: 'david_mark'
     , hostname: 'intertubes'
     , servername: 'tube001'
     , realname: 'Prototype Bot'
@@ -31,7 +31,7 @@ commands =
   { about: "http://github.com/gf3/protobot"
   , accessproperty: "https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Operators/Member_Operators"
   , anyone: "Has anyone really been far even as decided to use even go want to do look more like?"
-  , appendscript: "var script = document.createElement('script'); script.src='...'; document.body.appendChild(script);"
+  , appendscript: "var script = document.createElement( 'script' ); script.src='...'; document.body.appendChild( script );"
   , asi: "Automatic Semi-colon Insertion. Read: http://inimino.org/~inimino/blog/javascript_semicolons"
   , backtrace: "THE CONSEQUENCES WILL NEVER BE THE SAME"
   , bracketnotation: "https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Operators/Member_Operators#Bracket_notation"
@@ -44,21 +44,21 @@ commands =
   , dotnotation: "https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Operators/Member_Operators#Dot_notation"
   , DRY: "Don't Repeat Yourself"
   , ES3: "ES3 is edition 3 of ECMA-262, the ECMAScript specification: http://www.ecma-international.org/publications/standards/Ecma-262-arch.htm now obsoleted by ES5"
-  , ES5: "ES5 is edition 5 of ECMA-262, the ECMAScript (aka JavaScript) specification: http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-262.pdf"
+  , ES5: "ES5 is edition 5 of ECMA-262, the ECMAScript ( aka JavaScript ) specification: http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-262.pdf"
   , eventintro: "http://www.quirksmode.org/js/introevents.html"
   , evil: "eval is evil! Don't! Read: http://blogs.msdn.com/b/ericlippert/archive/2003/11/01/53329.aspx and http://blogs.msdn.com/b/ericlippert/archive/2003/11/04/53335.aspx"
   , help: "NO U!"
-  , ijsc: "☁ It's so just Cloud™ - http://itssojustcloud.com/ - http://groups.google.com/group/jquery-dev/browse_thread/thread/6a39be05b8477401#msg_f90341223bb9b68b ☁"
+  , isjc: "☁ It's so just Cloud™ - http://itssojustcloud.com/ - http://groups.google.com/group/jquery-dev/browse_thread/thread/6a39be05b8477401#msg_f90341223bb9b68b ☁"
   , jic: "just in case™"
   , jsis: "javascript is javascript is javascript"
-  , 'false': 'falsy values in js: null, undefined, NaN, false, zero (the number 0 - "0" is true), "" (empty string)'
+  , 'false': 'falsy values in js: null, undefined, NaN, false, zero ( the number 0 - "0" is true ), "" ( empty string )'
   , fouc: "http://paulirish.com/2009/avoiding-the-fouc-v3/"
   , minimal: "A minimal test case should contain precisely the HTML and JavaScript necessary to demonstrate the problem, no more and no less.  If it is more than 32 lines, it is probably not a minimal test case."
   , ninja: "http://ejohn.org/apps/learn"
   , noob: "http://www.marriedtothesea.com/022310/i-hate-thinking.gif"
   , os: "oh snao™"
   , osb: "oh snao™ bitch"
-  , osisjc: "( oh snao™ is so just cloud™ )™"
+  , osisjc: "(  oh snao™ is so just cloud™  )™"
   , pastie: "Paste links not code: http://pastie.org/ , http://jsbin.com/ , http://dpaste.de/ , http://gist.github.com/"
   , PHP: "You're asking a JavaScript question but you're showing us PHP instead of HTML and JavaScript. Maybe your PHP code results in well-formed JavaScript code, maybe it doesn't; we don't know. Please show us the HTML JavaScript that the browser sees."
   , plugins: "Check out: http://scripteka.com and http://livepipe.net"
@@ -66,15 +66,15 @@ commands =
   , protoquery: "STOP! Don't do it. Prototype and jQuery do the same things, you don't need both. It just adds twice the overhead and potential for conflicts. Pick one or the other."
   , proto: "http://dhtmlkitchen.com/learn/js/enumeration/prototype-chain.jsp"
   , reinvent: "We will not help you reinvent the wheel if we recommend using the many wheels already available. If you choose to make your own, you're on your own."
-  , sop: "Requests must respect the Same Origin Policy (http://en.wikipedia.org/wiki/Same_origin_policy). Requesting cross-domain content in javascript is generally prohibited. Seeing OPTIONS requests? See https://developer.mozilla.org/en/HTTP_access_control"
+  , sop: "Requests must respect the Same Origin Policy ( http://en.wikipedia.org/wiki/Same_origin_policy ). Requesting cross-domain content in javascript is generally prohibited. Seeing OPTIONS requests? See https://developer.mozilla.org/en/HTTP_access_control"
   , spelling: "Spelling and capitalization are important in programming."
   , testcase: "see: minimal"
   , tias: "Try It And See"
   , truthy: "Truthy/Falsy Values & Comparison Operators: http://www.sitepoint.com/blogs/2009/07/01/javascript-truthy-falsy/ Truthy/Falsy Values & Boolean Operator Results: http://11heavens.com/falsy-and-truthy-in-javascript"
-  , validid: 'ID attributes must begin with a letter ([A-Za-z]) and may be followed by any number of letters, digits ([0-9]), hyphens ("-"), underscores ("_"), colons (":"), and periods ("."). http://www.w3.org/TR/html401/types.html#h-6.2 - furthermore, IDs are unique, meaning only one element in the DOM can have a given ID at any time'
+  , validid: 'ID attributes must begin with a letter ( [A-Za-z] ) and may be followed by any number of letters, digits ( [0-9] ), hyphens ( "-" ), underscores ( "_" ), colons ( ":" ), and periods ( "." ). http://www.w3.org/TR/html401/types.html#h-6.2 - furthermore, IDs are unique, meaning only one element in the DOM can have a given ID at any time'
   , vamp: "http://slash7.com/pages/vampires"
   , WET: "Write Everything Twice"
-  , '(?:gl|glwtd)': "http://goodluckwiththatdude.com/"
+  , '( ?:gl|glwtd )': "http://goodluckwiththatdude.com/"
   , '===': "For any primitive values o and p, o === p if o and p have the same value and type.  For any Objects o and p, o === p if mutating o will mutate p in the same way."
   }
 
@@ -92,73 +92,82 @@ wat =
   , 'If I choose lots of files tactics, then I become to have directory traversal security problem?'
   ]
 
-for (c in commands) {
-  jerk(function(j) {
+for ( c in commands ) {
+  jerk( function( j ) {
     var cmd = commands[c]
-    j.watch_for(new RegExp("^" + c + "(?:\\s*@\\s*([-\\[\\]|_\\w]+))?\\s*$", "i"), function(message) {
-      message.say(to(message) + ": " + cmd)
+    j.watch_for( new RegExp( "^" + c + "(?:\\s*@\\s*( [-\\[\\]|_\\w]+))?\\s*$", "i" ), function( message ) {
+      message.say( to( message ) + ": " + cmd )
     })
   })
 }
 
 /* ------------------------------ Protobot ------------------------------ */
-jerk(function(j) {
-  j.watch_for('debug', function(message) {
-    message.say(lolwat())
+jerk( function( j ) {
+  j.watch_for( 'debug', function( message ) {
+    message.say( lolwat() )
   })
 
-  j.watch_for(/\bw[au]t\b/, function(message) {
-    message.say(wat[Math.floor(Math.random() * wat.length)])
+  j.watch_for( /\bw[au]t\b/, function( message ) {
+    message.say( wat[Math.floor( Math.random(  ) * wat.length )] )
   })
   
-  j.watch_for(/^(?:hi|hello|hey)$/i, function(message) {
-    message.say(message.user + ": oh hai!")
+  j.watch_for( /^(?:hi|hello|hey)$/i, function( message ) {
+    message.say( message.user + ": oh hai!" )
   })
 
-  j.watch_for(/^((?:NO )+)U$/, function(message) {
-    message.say(message.user + ": " + message.match_data[1] + "NO U")
+  j.watch_for( /^((?:NO )+)U$/, function( message ) {
+    message.say( message.user + ": " + message.match_data[1] + "NO U" )
   })
   
-  j.watch_for(/^eval (.+)/, function(message){
-    sandbox.run(message.match_data[1], function(output) { var original_length
-      if ((original_length = output.length) > (1024 - message.user.length - 3))
-        output = output.slice(0, 768) + '  (' + (original_length - 768) + ' characters truncated)'
-      message.say(message.user + ": " + output)
+  j.watch_for( /^eval (.+)/, function( message ){
+    sandbox.run( message.match_data[1], function( output ) { var original_length
+      if ( ( original_length = output.length ) > ( 1024 - message.user.length - 3 ) )
+        output = output.slice( 0, 768 ) + '  ( ' + ( original_length - 768 ) + ' characters truncated )'
+      message.say( message.user + ": " + output )
     })
   })
   
-  j.watch_for(/^(?:it )?doesn(?:')?t work(?:\s*@\s*([-\[\]|_\w]+))?/, function(message) {
-    message.say(to(message, "doesn't work") + ": What do you mean it doesn't work?  What happens when you try to run it?  What's the output?  What's the error message?  Saying \"it doesn't work\" is pointless.")
+  j.watch_for( /^(?:it )?doesn(?:')?t work(?:\s*@\s*([-\[\]|_\w]+))?/, function( message ) {
+    message.say( to( message, "doesn't work" ) + ": What do you mean it doesn't work?  What happens when you try to run it?  What's the output?  What's the error message?  Saying \"it doesn't work\" is pointless." )
   })
   
-  j.watch_for(/^g(?:oogle)? ([^@]+)(?:\s*@\s*([-\[\]|_\w]+))?/, function(message) {
-    var user = to(message, 2)
-    google.search(message.match_data[1], function(results) {
-      if (results.length) message.say(user + ": " + results[0].titleNoFormatting + " - " + results[0].unescapedUrl)
-      else message.say(user + ": Sorry, no results for '" + message.match_data[1] + "'")
+  j.watch_for( /^g(?:oogle)? ([^@]+)(?:\s*@\s*([-\[\]|_\w]+))?(?:\s*#(\d))?$/, function( message ) {
+    var user = to( message, 2 )
+      , res  = message.match_data[2] || 0
+    google.search( message.match_data[1], function( results ) {
+      if ( results.length )
+        message.say( user + ": " + results[res].titleNoFormatting + " - " + results[res].unescapedUrl )
+      else 
+        message.say( user + ": Sorry, no results for '" + message.match_data[1] + "'" )
     })
   })
   
-  j.watch_for(/^mdc ([^@]+)(?:\s*@\s*([-\[\]|_\w]+))?/, function(message) {
-    var user = to(message, 2)
-    google.search(message.match_data[1] + ' site:developer.mozilla.org', function(results) {
-      if (results.length) message.say(user + ": " + results[0].titleNoFormatting + " - " + results[0].unescapedUrl)
-      else message.say(user + ": Sorry, no results for '" + message.match_data[1] + "'")
+  j.watch_for( /^mdc ([^@]+)(?:\s*@\s*([-\[\]|_\w]+))?(?:\s*#(\d))?$/, function( message ) {
+    var user = to( message, 2 )
+      , res  = message.match_data[2] || 0
+    google.search( message.match_data[1] + ' site:developer.mozilla.org', function( results ) {
+      if ( results.length )
+        message.say( user + ": " + results[res].titleNoFormatting + " - " + results[res].unescapedUrl )
+      else
+        message.say( user + ": Sorry, no results for '" + message.match_data[1] + "'" )
     })
   })
   
-  j.watch_for(/^api ([$\w]+(?:[\.#]\w+)*)(?:\s+@\s*([-\[\]|_\w]+))?/, function(message) {
-    message.say(to(message, 2) + ": Sorry, the `api` command is temporarily disabled. Docs here: http://api.prototypejs.org/")
+  j.watch_for( /^api ([$\w]+(?:[\.#]\w+)*)(?:\s+@\s*([-\[\]|_\w]+))?/, function( message ) {
+    message.say( to( message, 2 ) + ": Sorry, the `api` command is temporarily disabled. Docs here: http://api.prototypejs.org/" )
   })
-}).connect(options)
+}).connect( options )
 
 /* ------------------------------ Functions ------------------------------ */
-function lolwat() {
+function lolwat () {
   return "LOLWAT"
 }
 
-function to(message, def, idx) {
-  if (typeof idx === 'undefined' && typeof def === 'number') idx = def, def = null
-  else idx = idx || 1
+function to ( message, def, idx ) {
+  if ( typeof idx === 'undefined' && typeof def === 'number' )
+    idx = def, def = null
+  else
+    idx = idx || 1
   return !!message.match_data[idx] ? message.match_data[idx] : def || message.user
 }
+
