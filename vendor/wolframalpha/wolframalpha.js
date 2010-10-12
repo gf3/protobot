c64143564d47a2fa5feb59a036810e6faf79338a
Ali - Server <ali@karbassi.com>
2010-10-12 13:09:03 -0500

    Added url

--- vendor/wolframalpha/wolframalpha.js
+++ vendor/wolframalpha/wolframalpha.js
@@ -4,22 +4,22 @@ var exec = require('child_process').exec;
 /* ------------------------------ WolframAlpha ------------------------ */
 function WolframAlpha() {
     this.search = function(query, hollaback) {
-        exec("curl -e 'http://www.wolframalpha.com' 'http://www.wolframalpha.com/input/?i=" + encodeURIComponent(query) + "'", function (err, stdout, stderr) {
-            var result,
-                solution = />Solution:<[\s\S]*?alt\s*=\s*\"([^\""]*)\"/,
+		var result = {
+			url: "http://www.wolframalpha.com/input/?i=" + encodeURIComponent(query)
+		};
+
+        exec("curl -e 'http://www.wolframalpha.com' '" + result.url + "'", function (err, stdout, stderr) {
+            var solution = />Solution:<[\s\S]*?alt\s*=\s*\"([^\""]*)\"/,
                 other = /stringified"\s*:\s*"([^"\r\n]*)/g;
 
             if (solution.test(stdout)) {
-                result = stdout.match(solution)[1];
+                result.data = stdout.match(solution)[1];
             } else {
-                result = stdout.match(other);
-                if (!result) {
-                    result = null;
+                match = stdout.match(other);
+                if (!match) {
+                    result.data = null;
                 } else {
-                    result = result[1];
-                    result = result.replace(/stringified"\s*:\s*"/g, '');
-                    // result = result.replace(/\s*\|\s.*/, '');
-                    result = result.replace(/\\n/g, ' ');
+                    result.data = match[1].replace(/stringified"\s*:\s*"/g, '').replace(/\\n/g, ' ');
                 }
             }
 
