5f44f638edffb525bcdff88b23d0b085e339d2d4
unknown <Cybernetium@.(none)>
2010-10-21 14:36:55 -0400

    more webspiderch

--- protobot.js
+++ protobot.js
@@ -110,8 +110,9 @@ wat =
   , 'The new icon changes your desktop look really professional!'
   , 'Because in "normal" js is only getElementById which even not working well no normal getElementByClass'
   , 'For some reason this doesn\'t sign in when I barely start using it and when i try to go to it and it isn\'t signed it, I can\'t sign in on that page.'
-  , 'can  i use document.write to call a json'
+  , 'can i use document.write to call a json'
   , 'once you understand it will be a red head to wash with dirty waters.. no problem enjoy'
+  , 'life is hard...  be alive is funny... sufferin for other happyness is a non sense'
   ]
 
 for ( c in commands ) {
