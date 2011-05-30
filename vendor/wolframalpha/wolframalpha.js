06f08796893c33406f0822e3f58dad1ba5833488
Gianni Chiappetta <gianni@runlevel6.org>
2011-05-30 14:04:53 -0400

    stuff

--- vendor/wolframalpha/wolframalpha.js
+++ vendor/wolframalpha/wolframalpha.js
@@ -1,31 +1,33 @@
 /* ------------------------------ Includes && Options ----------------- */
-var exec = require('child_process').exec;
+var exec = require( 'child_process' ).exec
 
 /* ------------------------------ WolframAlpha ------------------------ */
 function WolframAlpha() {
-    this.search = function(query, hollaback) {
-		var result = {
-			url: "http://www.wolframalpha.com/input/?i=" + encodeURIComponent(query)
-		};
+    this.search = function( query, hollaback ) {
+    var result =
+      { url: 'http://www.wolframalpha.com/input/?i=' + encodeURIComponent( query )
+      }
 
-        exec("curl -e 'http://www.wolframalpha.com' '" + result.url + "'", function (err, stdout, stderr) {
-            var solution = />Solution:<[\s\S]*?alt\s*=\s*\"([^\""]*)\"/,
-                other = /stringified"\s*:\s*"([^"\r\n]*)/g;
+      exec( "curl -e 'http://www.wolframalpha.com' '" + result.url + "'", function ( err, stdout, stderr ) {
+        var solution = />Solution:<[\s\S]*?alt\s*=\s*\"([^\""]*)\"/
+          , other = /stringified"\s*:\s*"([^"\r\n]*)/g
 
-            if (solution.test(stdout)) {
-                result.data = stdout.match(solution)[1];
-            } else {
-                match = stdout.match(other);
-                if (!match) {
-                    result.data = null;
-                } else {
-                    result.data = match[1].replace(/stringified"\s*:\s*"/g, '').replace(/\\n/g, ' ');
-                }
-            }
+        if ( solution.test( stdout ) )
+          result.data = stdout.match( solution )[1]
+        else {
+          match = stdout.match( other )
+          if ( !match )
+            result.data = null
+          else
+            result.data = match[1]
+              .replace( /stringified"\s*:\s*"/g, '' )
+              .replace( /\\n/g, ' ' )
+              .replace( /\//, '/' )
+        }
 
-            hollaback.call(this, result);
-        });
-    };
+        hollaback.call( this, result )
+    })
+  }
 }
 
 /* ------------------------------ Export ------------------------------ */
