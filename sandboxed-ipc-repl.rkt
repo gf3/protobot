62a50794c01375474a08c407e20919bfb18eaaf7
Gianni Chiappetta <gianni@runlevel6.org>
2011-07-05 12:40:11 -0400

    added: Scheme join function.

--- sandboxed-ipc-repl.rkt
+++ sandboxed-ipc-repl.rkt
@@ -3,4 +3,10 @@
 (require racket/pretty)
 
 (define e (make-evaluator 'racket/base '(require racket/list)))
+
+(e '(define (join lst str)
+  (let jn ([l lst] [a ""])
+    (cond [(null? (cdr l)) (string-append a (car l))]
+          [else (jn (cdr l) (string-append a (car l) str))]))))
+
 (pretty-display (e (read)))
