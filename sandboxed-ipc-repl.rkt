96d451858af29a0444918e2aa02876ff4758b7cc
Gianni Chiappetta <gianni@runlevel6.org>
2011-07-07 12:42:42 -0400

    NO U

--- sandboxed-ipc-repl.rkt
+++ sandboxed-ipc-repl.rkt
@@ -2,7 +2,7 @@
 (require racket/sandbox)
 (require racket/pretty)
 
-(define e (make-evaluator 'racket/base '(require racket/list)))
+(define e (make-evaluator 'racket/base '(require racket/list) '(require racket/vector) '(require racket/function)))
 
 (e '(define (join lst str)
   (let jn ([l lst] [a ""])
