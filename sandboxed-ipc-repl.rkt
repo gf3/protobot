f9b161048ad3bdfc72fc07c8f0a0b0e32abfe2c0
Gianni Chiappetta <gianni@runlevel6.org>
2011-07-19 21:47:54 -0400

    updated: Added range to sandbox.

--- sandboxed-ipc-repl.rkt
+++ sandboxed-ipc-repl.rkt
@@ -2,11 +2,17 @@
 (require racket/sandbox)
 (require racket/pretty)
 
-(define e (make-evaluator 'racket/base '(require racket/list) '(require racket/vector) '(require racket/function)))
+(define e (make-evaluator 'racket/base '(require racket/list)
+                                       '(require racket/vector)
+                                       '(require racket/function)))
 
 (e '(define (join lst str)
   (let jn ([l lst] [a ""])
     (cond [(null? (cdr l)) (string-append a (car l))]
           [else (jn (cdr l) (string-append a (car l) str))]))))
+
+(e '(define (.. start end)
+  (let ([diff (- end start)])
+    (build-list diff (λ (n) (+ n start))))))
 
 (pretty-write (e (read)))
