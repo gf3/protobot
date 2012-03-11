9126d1dc8232162d47f00d431ffb517e77cd6778
nlogax <jonas.westerlund+github@gmail.com>
2012-03-11 18:57:27 +0100

    Fix DuckDuckGo

--- plugins/duckduckgo.js
+++ plugins/duckduckgo.js
@@ -8,18 +8,20 @@ function DuckDuckGo() {
 
 DuckDuckGo.prototype.search = function( query, cloudback ) {
   exec("curl 'http://api.duckduckgo.com/?format=json&q=" + escape( query ) + "'", function ( err, stdout, stderr ) {
-    var results = JSON.parse( stdout )
+    var results = JSON.parse( stdout ) // What could possibly go wrong?
     cloudback.call( this, results )
   })
 }
 
 function register( j ) {
+  var ddg = new DuckDuckGo()
+
   j.watch_for( /^([\/.,`?]?)ddg ([^#@]+)(?:\s*#([1-9]))?(?:\s*@\s*([-\[\]\{\}`|_\w]+))?$/, function( message ) {
     var user = message.match_data[4] || message.name
       , term = message.match_data[2]
       , num  = +message.match_data[3]-1 || 0
 
-    duckDuckGo.search( term, function( results ) {
+    ddg.search( term, function( results ) {
       if ( results["AbstractText"] )
         message.say( user + ': ' + unescapeAll( results["AbstractText"] ) + ' - ' + results["AbstractURL"] )
       else if ( results["Definition"] )
